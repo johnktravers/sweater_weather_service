25b977e2783f4d0429a3e661ee485c860e0b7047
John Travers <johnk.travers@gmail.com>
2020-01-12 22:27:36 -0700

    Add test for forecast endpoint with given location parameter

---
+++ spec/requests/api/v1/forecast_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast API Endpoint' do
+  it 'can return a city\'s weather forecast' do
+    get '/api/v1/forecast?location=denver,co', headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response).to be_successful
+
+    forecast_info = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(forecast_info[:type]).to eq('forecast')
+    expect(forecast_info[:attributes][:address]).to eq('Denver, CO, USA')
+
+    current_keys = [:time, :summary, :icon, :temp, :apparent_temp, :humidity, :visibility, :uv_index]
+    expect(forecast_info[:attributes][:current_weather].keys).to eq(current_keys)
+
+    hourly_keys = [:time, :summary, :icon, :temp]
+    expect(forecast_info[:attributes][:hourly_weather][0].keys).to eq(hourly_keys)
+    expect(forecast_info[:attributes][:hourly_weather].length).to eq(25)
+
+    daily_keys = [:time, :summary, :icon, :temp_high, :temp_low, :precip_probability, :precip_type]
+    expect(forecast_info[:attributes][:daily_weather][0].keys).to eq(daily_keys)
+    expect(forecast_info[:attributes][:daily_weather].length).to eq(8)
+  end
+end
