61739c89a93606f435590b0ef1f8baa910f4906a
John Travers <johnk.travers@gmail.com>
2020-01-13 11:33:11 -0700

    Add error handling to show action of antipodes controller

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -3,9 +3,13 @@ class Api::V1::AntipodesController < ApplicationController
     geocode = get_geocode(params[:location])
     antipode = get_antipode(geocode)
     get_antipode_name(antipode)
-    get_antipode_forecast(antipode)
-    serialized_antipode = AntipodeSerializer.new(antipode)
-    render json: serialized_antipode
+    if antipode.location_name
+      get_antipode_forecast(antipode)
+      serialized_antipode = AntipodeSerializer.new(antipode)
+      render json: serialized_antipode
+    else
+      render json: antipode_error, status: 404
+    end
   end
 
   private
@@ -32,5 +36,14 @@ class Api::V1::AntipodesController < ApplicationController
     dark_sky_service = DarkSkyService.new
     forecast_info = dark_sky_service.get_forecast_info(antipode.lat, antipode.long)
     antipode.set_forecast(forecast_info)
+  end
+
+  def antipode_error
+    { errors:
+      [{
+        status: '404 Not Found',
+        title: 'The given location does not have an existing antipode. Please try another location.'
+      }]
+    }.to_json
   end
 end
