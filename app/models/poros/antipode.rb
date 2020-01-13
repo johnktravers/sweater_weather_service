ad2eedddc31bd14235629327bffe76ce37f3e871
John Travers <johnk.travers@gmail.com>
2020-01-13 10:51:43 -0700

    Add id and find_city_name private method to antipode PORO

--- app/models/poros/antipode.rb
+++ app/models/poros/antipode.rb
@@ -1,7 +1,8 @@
 class Antipode
-  attr_reader :lat, :long, :search_location, :location_name, :forecast
+  attr_reader :id, :lat, :long, :search_location, :location_name, :forecast
 
   def initialize(info, search_location)
+    @id = 1
     @lat = info[:attributes][:lat]
     @long = info[:attributes][:long]
     @search_location = search_location
@@ -10,10 +11,20 @@ class Antipode
   end
 
   def set_location_name(info)
-    @location_name = info[:results][-2][:formatted_address]
+    address = find_city_name(info)
+    @location_name = address
   end
 
   def set_forecast(info)
     @forecast = Forecast.new(info, location_name)
+  end
+
+  private
+
+  def find_city_name(info)
+    info[:results].find do |address_info|
+      address_types = address_info[:address_components][0][:types]
+      address_types.include?('locality') || address_types.include?('administrative_area_level_1')
+    end[:formatted_address]
   end
 end
