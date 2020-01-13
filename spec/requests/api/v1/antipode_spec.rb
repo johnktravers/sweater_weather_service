bc8b0b538c3a6915337fa93433dbee7f1c7672b8
John Travers <johnk.travers@gmail.com>
2020-01-13 10:51:06 -0700

    Modify test to match API endpoint output

--- spec/requests/api/v1/antipode_spec.rb
+++ spec/requests/api/v1/antipode_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Antipode API Endpoint' do
   it 'returns the antipode city name and forecast for a given city' do
-    get '/api/v1/antipode?location=hong kong, china', headers: {
+    get '/api/v1/antipode?location=hong kong', headers: {
       'Content-Type' => 'application/json',
       'Accept' => 'application/json'
     }
 
     expect(response).to be_successful
 
-    antipode_info = JSON.parse(response.body, symbolize_names: true)[:data][0]
+    antipode_info = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(antipode_info[:id]).to eq(1)
-    expect(antipode_info[:attributes][:location_name]).to eq('La Quiaca, Argentina')
-    expect(antipode_info[:attributes][:search_location]).to eq('Hong Kong, China')
+    expect(antipode_info[:id]).to eq('1')
+    expect(antipode_info[:attributes][:location_name]).to eq('Jujuy, Argentina')
+    expect(antipode_info[:attributes][:search_location]).to eq('Hong Kong')
 
-    forecast_keys = [:summary, :current_temperature]
-    expect(antipode_info[:attributes][:forecast][:keys]).to eq(forecast_keys)
+    forecast_keys = [:summary, :current_temp]
+    expect(antipode_info[:attributes][:forecast].keys).to eq(forecast_keys)
   end
 end
