a5c72a0726a5cbb260310f6e31933271a0f398b4
John Travers <johnk.travers@gmail.com>
2020-01-13 10:18:36 -0700

    Add get_location_info method to google geocoding service

--- app/services/google_geocoding_service.rb
+++ app/services/google_geocoding_service.rb
@@ -1,10 +1,26 @@
 class GoogleGeocodingService
   def get_geocode_info(location)
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
-      req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
+    response = conn.get('json') do |req|
       req.params['address'] = location
     end
 
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_location_info(lat, long)
+    response = conn.get('json') do |req|
+      req.params['latlng'] = "#{lat},#{long}"
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(
+      url: 'https://maps.googleapis.com/maps/api/geocode',
+      params: { key: ENV['GOOGLE_GEOCODING_API_KEY'] }
+    )
   end
 end
