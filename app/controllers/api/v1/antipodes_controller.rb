54b7afb52ce9223d6d8bfbf7eedbbfa05f0f0e5c
John Travers <johnk.travers@gmail.com>
2020-01-13 11:42:56 -0700

    Add memoization to avoid repeating API calls

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -16,25 +16,25 @@ class Api::V1::AntipodesController < ApplicationController
 
   def get_geocode(location)
     geocode_service = GoogleGeocodingService.new
-    geocode_info = geocode_service.get_geocode_info(location)
+    @geocode_info ||= geocode_service.get_geocode_info(location)
     Geocode.new(geocode_info)
   end
 
   def get_antipode(geocode)
     amypode_service = AmypodeService.new
-    antipode_info = amypode_service.get_antipode_info(geocode)
+    @antipode_info ||= amypode_service.get_antipode_info(geocode)
     Antipode.new(antipode_info, geocode.address)
   end
 
   def get_antipode_name(antipode)
     geocode_service = GoogleGeocodingService.new
-    location_info = geocode_service.get_location_info(antipode.lat, antipode.long)
+    @location_info ||= geocode_service.get_location_info(antipode.lat, antipode.long)
     antipode.set_location_name(location_info)
   end
 
   def get_antipode_forecast(antipode)
     dark_sky_service = DarkSkyService.new
-    forecast_info = dark_sky_service.get_forecast_info(antipode.lat, antipode.long)
+    @forecast_info ||= dark_sky_service.get_forecast_info(antipode.lat, antipode.long)
     antipode.set_forecast(forecast_info)
   end
 
