ff667d2b60f6e786ea64c8e80afd05905dac4af5
John Travers <johnk.travers@gmail.com>
2020-01-13 16:20:54 -0700

    Add sad path tests for user registration

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -18,4 +18,65 @@ RSpec.describe 'User Registration API Endpoint' do
 
     expect(user_key[:api_key].length).to eq(32)
   end
+
+  it 'displays an error message if the email is already taken' do
+    create(:user, email: 'whatever@example.com')
+    req_body = '{
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }'
+    post '/api/v1/users', params: req_body, headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response.status).to eq(400)
+
+    error = JSON.parse(response.body, symbolize_names: true)[:errors]
+
+    expect(error.count).to eq(1)
+    expect(error[0][:status]).to eq('400 Bad Request')
+    expect(error[0][:title]).to eq('Email has already been taken')
+  end
+
+  it 'displays an error message if the passwords do not match' do
+    req_body = '{
+      "email": "whatever@example.com",
+      "password": "password1",
+      "password_confirmation": "password2"
+    }'
+    post '/api/v1/users', params: req_body, headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response.status).to eq(400)
+
+    error = JSON.parse(response.body, symbolize_names: true)[:errors]
+
+    expect(error.count).to eq(1)
+    expect(error[0][:status]).to eq('400 Bad Request')
+    expect(error[0][:title]).to eq("Password confirmation doesn't match Password")
+  end
+
+  it 'displays an error message if one of the fields is blank' do
+    req_body = '{
+      "password_confirmation": "password2"
+    }'
+    post '/api/v1/users', params: req_body, headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response.status).to eq(400)
+
+    error = JSON.parse(response.body, symbolize_names: true)[:errors]
+
+    expect(error.count).to eq(2)
+    expect(error[0][:status]).to eq('400 Bad Request')
+    expect(error[0][:title]).to eq("Email can't be blank")
+    expect(error[1][:status]).to eq('400 Bad Request')
+    expect(error[1][:title]).to eq("Password can't be blank")
+  end
 end
