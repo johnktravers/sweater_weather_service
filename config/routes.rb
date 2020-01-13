0edb7c629ca5ec971f967c147d5faa4782079ee0
John Travers <johnk.travers@gmail.com>
2020-01-12 23:24:33 -0700

    Add namespaced route to user registration endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecasts#show'
+      get  '/forecast', to: 'forecasts#show'
+      post '/users',    to: 'users#create'
     end
   end
 end
