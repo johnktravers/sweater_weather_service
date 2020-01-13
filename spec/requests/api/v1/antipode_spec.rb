2fd7493c1833f269f5c1324e633214deeec7d6fe
John Travers <johnk.travers@gmail.com>
2020-01-13 10:58:56 -0700

    Add test that antipode endpoint works for other cities

--- spec/requests/api/v1/antipode_spec.rb
+++ spec/requests/api/v1/antipode_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe 'Antipode API Endpoint' do
     forecast_keys = [:summary, :current_temp]
     expect(antipode_info[:attributes][:forecast].keys).to eq(forecast_keys)
   end
+
+  it 'returns valid information for another city with an antipode' do
+    get '/api/v1/antipode?location=jaen, spain', headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response).to be_successful
+
+    antipode_info = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(antipode_info[:id]).to eq('1')
+    expect(antipode_info[:attributes][:location_name]).to eq('Waitao, New Zealand')
+    expect(antipode_info[:attributes][:search_location]).to eq('Jaén, Spain')
+
+    forecast_keys = [:summary, :current_temp]
+    expect(antipode_info[:attributes][:forecast].keys).to eq(forecast_keys)
+  end
 end
