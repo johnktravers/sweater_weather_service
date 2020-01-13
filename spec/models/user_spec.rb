ec948b19ec44dadae5e34595d01c1de3411f11a4
John Travers <johnk.travers@gmail.com>
2020-01-12 23:22:56 -0700

    Add user model validation tests for email and api key

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :email}
+    it { should validate_uniqueness_of :email}
+    it { should validate_presence_of :api_key }
+    it { should validate_uniqueness_of :api_key }
+    it { should validate_length_of(:api_key).is_equal_to(32) }
+  end
+end
