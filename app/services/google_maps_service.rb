60a1f3fab7762a96426e93b3140ff31fc5247595
John Travers <johnk.travers@gmail.com>
2020-01-14 14:52:51 -0700

    Add google maps service with get_directions_info method

---
+++ app/services/google_maps_service.rb
@@ -0,0 +1,11 @@
+class GoogleMapsService
+  def get_directions_info(origin, destination)
+    response = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |req|
+      req.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
+      req.params['origin'] = origin
+      req.params['destination'] = destination
+    end
+
+    JSON.parse(response.body, symbolize_names: true)[:routes].first[:legs].first
+  end
+end
