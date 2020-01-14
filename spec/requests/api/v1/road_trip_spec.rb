282d7c5ddc9711d9a8bed83ef67b94779501a971
John Travers <johnk.travers@gmail.com>
2020-01-14 15:11:02 -0700

    Add tests for error handling with invalid or missing api key

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -29,4 +29,41 @@ RSpec.describe 'Road Trip API Endpoint' do
     road_trip_keys = [:local_time, :temperature, :summary]
     expect(road_trip_info[:attributes][:arrival_forecast].keys).to eq(road_trip_keys)
   end
+
+  it 'displays an error if the api key is invalid' do
+    req_body = {
+      'origin' => 'Denver,CO',
+      'destination' => 'Pueblo, CO',
+            'api_key' => 'invalid_api_key'
+    }.to_json
+    post '/api/v1/road_trip', params: req_body, headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response.status).to eq(401)
+
+    error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+    expect(error[:status]).to eq('401 Unauthorized')
+    expect(error[:title]).to eq('Given API key is invalid. Please try again')
+  end
+
+  it 'displays an error if the api key is missing' do
+    req_body = {
+      'origin' => 'Denver,CO',
+      'destination' => 'Pueblo, CO'
+    }.to_json
+    post '/api/v1/road_trip', params: req_body, headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response.status).to eq(401)
+
+    error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+    expect(error[:status]).to eq('401 Unauthorized')
+    expect(error[:title]).to eq('Given API key is invalid. Please try again')
+  end
 end
