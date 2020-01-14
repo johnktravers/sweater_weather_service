27a0292873a699577beeec3266c020e6cb2bd4c8
John Travers <johnk.travers@gmail.com>
2020-01-14 14:53:16 -0700

    Add get_future_forecast_info method to dark sky service

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,10 +1,20 @@
 class DarkSkyService
   def get_forecast_info(lat, long)
-    url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}"
-    response = Faraday.get(url) do |req|
-      req.params['exclude'] = 'minutely,alerts,flags'
-    end
-    
+    response = conn.get("#{lat},#{long}")
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_future_forecast_info(lat, long, time)
+    response = conn.get("#{lat},#{long},#{time}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(
+      url: "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}",
+      params: { exclude: 'minutely,alerts,flags' }
+    )
   end
 end
