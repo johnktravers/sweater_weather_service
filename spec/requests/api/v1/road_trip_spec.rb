a6bef9029e325f79bdc2cce6de1f1ab2f8b2bc87
John Travers <johnk.travers@gmail.com>
2020-01-14 14:50:24 -0700

    Modify test to use factory and match road trip endpoint json response

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -2,22 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Road Trip API Endpoint' do
   before :each do
-    req_body = '{
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }'
-    post '/api/v1/users', params: req_body, headers: {
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json'
-    }
-    @user = User.last
+    @user = create(:user)
   end
 
   it 'can create a roadtrip for a user given a valid api key' do
     req_body = {
       'origin' => 'Denver,CO',
-      'destination' => 'Pueblo,CO',
+      'destination' => 'Pueblo, CO',
       'api_key' => @user.api_key
     }.to_json
     post '/api/v1/road_trip', params: req_body, headers: {
@@ -30,11 +21,12 @@ RSpec.describe 'Road Trip API Endpoint' do
     road_trip_info = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(road_trip_info[:type]).to eq('road_trip')
-    expect(forecast_info[:attributes][:origin]).to eq('Denver, CO, USA')
-    expect(forecast_info[:attributes][:destination]).to eq('Pueblo, CO, USA')
-    expect(forecast_info[:attributes][:travel_time]).to eq('2 hours')
+    expect(road_trip_info[:id]).to eq('1')
+    expect(road_trip_info[:attributes][:origin]).to eq('Denver, CO, USA')
+    expect(road_trip_info[:attributes][:destination]).to eq('Pueblo, CO, USA')
+    expect(road_trip_info[:attributes][:travel_time]).to eq('1 hour 48 mins')
 
-    forecast_keys = [:temp, :summary]
-    expect(forecast_info[:attributes][:arrival_forecast].keys).to eq(forecast_keys)
+    road_trip_keys = [:local_time, :temperature, :summary]
+    expect(road_trip_info[:attributes][:arrival_forecast].keys).to eq(road_trip_keys)
   end
 end
