606b4526e04719baed10a933bbc230993db65995
John Travers <johnk.travers@gmail.com>
2020-01-14 09:39:52 -0700

    Add validation of password confirmation to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :api_key }
     it { should validate_uniqueness_of :api_key }
     it { should validate_length_of(:api_key).is_equal_to(32) }
+    it { should validate_presence_of :password_confirmation }
   end
 end
