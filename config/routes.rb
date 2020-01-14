34b4006ca89f089548845de0f757eed124cc1c90
John Travers <johnk.travers@gmail.com>
2020-01-14 09:38:48 -0700

    Add route for user to create a new road trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get  '/forecast', to: 'forecasts#show'
-      post '/users',    to: 'users#create'
-      post '/sessions', to: 'sessions#create'
+      get  '/forecast',  to: 'forecasts#show'
+      post '/users',     to: 'users#create'
+      post '/sessions',  to: 'sessions#create'
+      post '/road_trip', to: 'road_trips#create'
     end
   end
 end
