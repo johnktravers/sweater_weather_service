e74e3815b9c1bc9f8ca555d8128a9969afdcb16b
John Travers <johnk.travers@gmail.com>
2020-01-14 21:30:46 -0700

    Configure rspec to use webmock and include the webmock helpers module

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'webmock/rspec'
+require './spec/support/webmock_helpers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -64,6 +66,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Configure the use of webmock helper methods
+  config.include WebmockHelpers
 
   # Configure tests to use factorybot syntax
   config.include FactoryBot::Syntax::Methods
