55367205bdb859a878a7d5a5e00342b45c6474d1
John Travers <johnk.travers@gmail.com>
2020-01-14 22:47:36 -0700

    Add get_uv_rating method to add context to integer value of uv rating

--- app/models/poros/current_weather.rb
+++ app/models/poros/current_weather.rb
@@ -7,6 +7,22 @@ class CurrentWeather < Weather
     @apparent_temp = info[:apparentTemperature]
     @humidity = info[:humidity]
     @visibility = info[:visibility]
-    @uv_index = info[:uvIndex]
+    @uv_index = get_uv_rating(info[:uvIndex])
+  end
+
+  private
+
+  def get_uv_rating(uv_index)
+    if uv_index < 2.5
+      "#{uv_index} " + '(low)'
+    elsif uv_index < 5.5
+      "#{uv_index} " + '(moderate)'
+    elsif uv_index < 7.5
+      "#{uv_index} " + '(high)'
+    elsif uv_index < 10.5
+      "#{uv_index} " + '(very high)'
+    else
+      "#{uv_index} " + '(extreme)'
+    end
   end
 end
