dfcffef7b5f66d11a05b8a3251e559a7d6b39aae
John Travers <johnk.travers@gmail.com>
2020-01-15 08:36:47 -0700

    Move time from weather superclass into subclasses, add string manipulation to temps

--- app/models/poros/current_weather.rb
+++ app/models/poros/current_weather.rb
@@ -1,16 +1,28 @@
 class CurrentWeather < Weather
-  attr_reader :temp, :apparent_temp, :humidity, :visibility, :uv_index
+  attr_reader :time,
+              :temp,
+              :apparent_temp,
+              :humidity,
+              :visibility,
+              :uv_index
 
   def initialize(info, timezone)
     super
-    @temp = info[:temperature]
-    @apparent_temp = info[:apparentTemperature]
-    @humidity = (info[:humidity] * 100).to_s + '%'
+    @time = parse_local_time(info[:time], timezone)
+    @temp = info[:temperature].round.to_s + '°'
+    @apparent_temp = info[:apparentTemperature].round.to_s + '°'
+    @humidity = (info[:humidity] * 100).to_i.to_s + '%'
     @visibility = info[:visibility].to_s + ' miles'
     @uv_index = get_uv_rating(info[:uvIndex])
   end
 
   private
+
+  def parse_local_time(unix_time, timezone)
+    utc_time = Time.at(unix_time).utc.to_datetime
+    local_time = timezone.utc_to_local(utc_time)
+    local_time.strftime('%l:%M %p, %m/%d')
+  end
 
   def get_uv_rating(uv_index)
     if uv_index < 2.5
--- app/models/poros/hourly_weather.rb
+++ app/models/poros/hourly_weather.rb
@@ -1,8 +1,17 @@
 class HourlyWeather < Weather
-  attr_reader :temp
+  attr_reader :time, :temp
 
   def initialize(info, timezone)
     super
-    @temp = info[:temperature]
+    @time = parse_local_time(info[:time], timezone)
+    @temp = info[:temperature].round.to_s + '°'
+  end
+
+  private
+
+  def parse_local_time(unix_time, timezone)
+    utc_time = Time.at(unix_time).utc.to_datetime
+    local_time = timezone.utc_to_local(utc_time)
+    local_time.strftime('%l %p')
   end
 end
