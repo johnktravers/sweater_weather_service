0aad5fcd6393a810cc943cdaebbbc8fb2c355d28
John Travers <johnk.travers@gmail.com>
2020-01-15 08:35:59 -0700

    Add today summary, tonight summary, high and low temps to forecast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,11 +1,12 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :address,
-             :timezone,
+  attributes :location,
+             :today_summary,
+             :tonight_summary,
+             :temp_high,
+             :temp_low,
              :current_weather,
              :hourly_weather,
              :daily_weather
-
-  attribute :timezone { |forecast| forecast.timezone.identifier }
 end
