0d820464e8637a95606b7c111d0f053eee9a8480
John Travers <johnk.travers@gmail.com>
2020-01-14 22:39:30 -0700

    Refactor forecasts controller using a forecast facade object

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,22 +1,7 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
-    geocode = get_geocode(params[:location])
-    forecast = get_forecast(geocode.lat, geocode.long, geocode.address)
+    forecast = ForecastFacade.new(params[:location]).create_forecast
     serialized_forecast = ForecastSerializer.new(forecast)
     render json: serialized_forecast
-  end
-
-  private
-
-  def get_geocode(location)
-    geocode_service = GoogleGeocodingService.new
-    geocode_info = geocode_service.get_geocode_info(location)
-    Geocode.new(geocode_info)
-  end
-
-  def get_forecast(lat, long, address)
-    dark_sky_service = DarkSkyService.new
-    forecast_info = dark_sky_service.get_forecast_info(lat, long)
-    Forecast.new(forecast_info, address)
   end
 end
